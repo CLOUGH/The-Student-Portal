bdad6ce1134e5a6f404bb6e75c1290575a29167e
Warren Clough <clough.warren@gmail.com>
2013-02-19 20:17:31 -0500

    Updates

---
+++ dashboard/create-user.php
@@ -0,0 +1,41 @@
+<?php
+    $user;
+    if(isset($_GET['id'])){
+        include_once("../common/library/user.php");
+        $user = new User($id);
+    }
+
+?>
+<script type="text/javascript" src="create-user.js"></script>
+
+<h2>User Management</h2>
+<form id="<?php echo !isset($user)?'save-user':'update-user'?>" >
+  <fieldset>
+    <legend>Add User</legend>
+
+    <?php if(isset($user)):?>
+        <label>ID</label>
+        <input  disabled id="disabledInput" type="text" name="id" value="<?php echo isset($user)?$user->getID():''?>">
+    <?php endif ?>
+
+    <label>User</label>
+    <input type="text" name="user_name" value="<?php echo isset($user)?$user->getUsername():''?>">
+
+    <label>Password</label>
+    <input type="text" name="password" value="<?php echo isset($user)?$user->getPassword():''?>">
+
+    <label>First Name</label>
+    <input type="text" name="first_name" value="<?php echo isset($user)?$user->getFirstname():''?>">
+
+    <label>Last Name</label>
+    <input type="text" name="last_name" value="<?php echo isset($user)?$user->getLastname():''?>">
+
+    <label>Type</label>
+    <input type="text" name="type" value="<?php echo isset($user)?$user->getType():''?>">
+
+    <label>Email</label>
+    <input type="text" name="simester" value="<?php echo isset($user)?$user->getEmail():''?>">
+
+  </fieldset>
+  <button type="submit" class="btn">Submit</button>
+</form>
