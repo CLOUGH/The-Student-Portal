a4118a32a83e3bb037268273fdcf0e257b57a3c4
Shane <shanec132006@gmail.com>
2013-02-20 18:48:54 -0500

    New Fron Slider

--- login/logout.php
+++ login/logout.php
@@ -1,6 +1,7 @@
 <?php
 include_once("../setup/config.php");
 session_start();
-session_destroy();
+unset($_SESSION['error_message']);
+unset($_SESSION['user_id']);
 header("Location:../index.php");
 ?>
